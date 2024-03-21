a3ef5f3a19e530dd72ff25f7736ad38b131d9a25
Nikita Mali <126570356+nikitasmali@users.noreply.github.com>
2024-03-21 16:58:02 +0530

    Add files via upload

---
+++ src/components/UI/AboutSection.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "../../styles/about-section.css";
+import aboutImg from "../../assets/all-images/cars-img/bmw-offer.png";
+
+const AboutSection = ({ aboutClass }) => {
+  return (
+    <section
+      className="about__section"
+      style={
+        aboutClass === "aboutPage"
+          ? { marginTop: "0px" }
+          : { marginTop: "280px" }
+      }
+    >
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="about__section-content">
+              <h4 className="section__subtitle">About Us</h4>
+              <h2 className="section__title">Welcome to car rent service</h2>
+              <p className="section__description">
+                We are dedicated to providing exceptional car rental services tailored to your needs. Our team strives to ensure a seamless and enjoyable experience for every customer. With a wide range of vehicles and flexible options, we're here to help you get where you need to go.
+              </p>
+
+              <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i>  Reliable transportation solutions.   
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Your seamless, enjoyable journey starts here.
+                </p>
+              </div>
+
+              <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Diverse fleet, tailored to your needs.
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Your satisfaction, our priority.
+                </p>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div className="about__img">
+              <img src={aboutImg} alt="" className="w-100" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/components/UI/Testimonial.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import Slider from "react-slick";
+
+import "../../styles/testimonial.css";
+
+import ava01 from "../../assets/all-images/ava-1.jpg";
+import ava02 from "../../assets/all-images/ava-2.jpg";
+import ava03 from "../../assets/all-images/ava-3.jpg";
+import ava04 from "../../assets/all-images/ava-4.jpg";
+
+const Testimonial = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    swipeToSlide: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <Slider {...settings}>
+      <div className="testimonial py-4 px-3">
+        <p className="section__description">
+        Booking our vacation through 'The Best Way to Give Trip' was an absolute game-changer. Their expert advice and personalized recommendations helped us create the perfect itinerary for our family getaway. From breathtaking landscapes to hidden gems off the beaten path, every moment was filled with wonder and adventure. Thank you for making our trip truly unforgettable!
+        </p>
+
+        <div className="mt-3 d-flex align-items-center gap-4">
+          <img src={ava01} alt="" className="w-25 h-25 rounded-2" />
+
+          <div>
+            <h6 className="mb-0 mt-3">Jhon Doe</h6>
+            <p className="section__description">Customer</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="testimonial py-4 px-3">
+        <p className="section__description">
+        Planning a trip can be overwhelming, but 'The Best Way to Give Trip' made it a breeze. Their user-friendly website and comprehensive travel resources made it easy to find inspiration and organize my itinerary. From booking accommodations to discovering off-the-beaten-path attractions, every aspect of my journey was seamlessly coordinated. I can't wait to use their services for my next adventure!
+        </p>
+
+        <div className="mt-3 d-flex align-items-center gap-4">
+          <img src={ava02} alt="" className="w-25 h-25 rounded-2" />
+
+          <div>
+            <h6 className="mb-0 mt-3">Sarah Johnson</h6>
+            <p className="section__description">Customer</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="testimonial py-4 px-3">
+        <p className="section__description">
+        Our family vacation wouldn't have been the same without the guidance of 'The Best Way to Give Trip.' Their expert team helped us navigate the complexities of traveling with kids, offering valuable advice and family-friendly recommendations. From finding accommodations with ample amenities to planning kid-friendly activities, they thought of everything to ensure our trip was enjoyable for the whole family. Thank you for making our vacation stress-free and unforgettable!
+        </p>
+
+        <div className="mt-3 d-flex align-items-center gap-4">
+          <img src={ava03} alt="" className="w-25 h-25 rounded-2" />
+
+          <div>
+            <h6 className="mb-0 mt-3">Robb Smith</h6>
+            <p className="section__description">Customer</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="testimonial py-4 px-3">
+        <p className="section__description">
+        I've always dreamt of traveling the world, but I never knew where to start. Thanks to 'The Best Way to Give Trip,' I was able to turn my dreams into reality. Their insightful blog posts, destination guides, and travel tips provided me with the confidence and knowledge I needed to embark on my solo adventure. From exploring bustling cities to immersing myself in local cultures, every moment was a step closer to fulfilling my wanderlust. Thank you for inspiring me to chase my travel dreams!"
+        </p>
+
+        <div className="mt-3 d-flex align-items-center gap-4">
+          <img src={ava04} alt="" className="w-25 h-25 rounded-2" />
+
+          <div>
+            <h6 className="mb-0 mt-3">Emily Brown</h6>
+            <p className="section__description">Customer</p>
+          </div>
+        </div>
+      </div>
+    </Slider>
+  );
+};
+
+export default Testimonial;
